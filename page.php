d770dfcf1a54e287c3213049947c0d97d8e08cf5
DzNiro <31928651+DzNiro@users.noreply.github.com>
2017-10-04 16:44:19 +0200

    Simon
    weiterführung Html/CSS Gerüst

---
+++ page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+    <title>Webaplikationsprojekt</title>
+  </head>
+  <body>
+	<div id="center">
+		<div id="top">
+		</div>
+		<div id="blog">
+			<div id="bild">
+			</div>
+			<div id="title">
+			</div>
+			<div id="beschreib">
+			</div>
+			<div id="trailer">
+			</div>
+			<div id="rating">
+			</div>
+		</div>
+		<div id="comment">
+			<div id="ucomment">
+			</div>
+		</div>
+	</div>
+  </body>
+</html>
